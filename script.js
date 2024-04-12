33ba09fbafa687cc99e90fb25e3d1b1e4591146b
heidithoi <hthoi4017@sdsu.edu>
2024-04-12 16:23:34 -0700

    Update to java

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+function darkMode() {
+    document.getElementsByTagName('body')[0].classList.toggle('body-dark-mode');
+
+    document.getElementsByClassName('star')[0].classList.toggle('star-dark-mode');
+    document.getElementsByClassName('star')[1].classList.toggle('star-dark-mode');
+    document.getElementsByClassName('star')[2].classList.toggle('star-dark-mode');
+    document.getElementsByClassName('star')[3].classList.toggle('star-dark-mode');
+    document.getElementsByClassName('star')[4].classList.toggle('star-dark-mode');
+    document.getElementsByClassName('star')[5].classList.toggle('star-dark-mode');
+    document.getElementsByClassName('star')[6].classList.toggle('star-dark-mode');
+    document.getElementsByClassName('star')[7].classList.toggle('star-dark-mode');
+    document.getElementsByClassName('star')[8].classList.toggle('star-dark-mode');
+
+    // document.getElementById('main-nav').classList.toggle('main-nav-dark-mode');
+
+    document.getElementsByClassName('home-button')[0].classList.toggle('home-button-dark-mode');
+
+    document.getElementsByClassName('home-button:hover')[0].classList.toggle('home-button-dark-mode:hover');
+
+    document.getElementsByClassName('header')[0].classList.toggle('header-dark-mode');
+
+    document.getElementsByClassName('hello_mh')[0].classList.toggle('hello_mh_dark_mode');
+
+    document.getElementsByClassName('heidi_mh')[0].classList.toggle('heidi_mh_dark_mode');
+
+    document.getElementsByClassName('aimage')[0].classList.toggle('aimage-dark-mode');
+
+    document.getElementsByClassName('tskills')[0].classList.toggle('tskills-dark-mode');
+    document.getElementsByClassName('tskills')[1].classList.toggle('tskills-dark-mode');
+    document.getElementsByClassName('tskills')[2].classList.toggle('tskills-dark-mode');
+    document.getElementsByClassName('tskills')[3].classList.toggle('tskills-dark-mode');
+    document.getElementsByClassName('tskills')[4].classList.toggle('tskills-dark-mode');
+    document.getElementsByClassName('tskills')[5].classList.toggle('tskills-dark-mode');
+    document.getElementsByClassName('tskills')[6].classList.toggle('tskills-dark-mode');
+    document.getElementsByClassName('tskills')[7].classList.toggle('tskills-dark-mode');
+
+    document.getElementsByClassName('personal')[0].classList.toggle('personal-dark-mode');
+
+    document.getElementById('gallery').classList.toggle('gallery-dark-mode');
+
+    document.getElementsByClassName('image')[0].classList.toggle('image-dark-mode');
+    document.getElementsByClassName('image')[1].classList.toggle('image-dark-mode');
+    document.getElementsByClassName('image')[2].classList.toggle('image-dark-mode');
+    document.getElementsByClassName('image')[3].classList.toggle('image-dark-mode');
+    document.getElementsByClassName('image')[4].classList.toggle('image-dark-mode');
+    document.getElementsByClassName('image')[5].classList.toggle('image-dark-mode');
+    document.getElementsByClassName('image')[6].classList.toggle('image-dark-mode');
+    document.getElementsByClassName('image')[7].classList.toggle('image-dark-mode');
+
+    document.getElementById('gallery_star').classList.toggle('gallery-star-dark-mode');
+
+    document.getElementsByClassName('faqs')[0].classList.toggle('faqs-dark-mode');
+    document.getElementsByClassName('faqs')[1].classList.toggle('faqs-dark-mode');
+    document.getElementsByClassName('faqs')[2].classList.toggle('faqs-dark-mode');
+    document.getElementsByClassName('faqs')[3].classList.toggle('faqs-dark-mode');
+
+    document.getElementsByClassName('button-primary')[0].classList.toggle('button-primary-dark-mode');
+    // document.getElementsByClassName('button-primary:hover')[0].classList.toggle('button-primary-dark-mode:hover');
+    }
+  
   function myMenu() {
     document.getElementById('mymenu').classList.toggle('open');
     document.getElementById('menuButton').classList.toggle('close');
